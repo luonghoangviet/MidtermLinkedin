ae4707a9abe305d24505306a28c3239bf311f86d
Luong Hoang Viet <luonghoangviet@gmail.com>
2016-05-06 16:39:47 +0700

    ver 5
    update js + html

--- source/script/myapp.js
+++ source/script/myapp.js
@@ -1,3 +1,6 @@
+
+
+
 var myapp= angular.module('myapp', []);
 
 myapp.controller('linkedin', ['$scope','$http',
@@ -17,4 +20,62 @@ myapp.controller('linkedin', ['$scope','$http',
             $scope.education = data.education;
             
         });
-    }]);
+    }]
+    
+    
+    
+    
+    );
+    
+    myapp.directive("clickToEdit", function () {
+    var editorTemplate = '' +
+        '<div class="click-to-edit">' +
+            '<div ng-hide="view.editorEnabled">' +
+                '{{value}} ' +
+                '<a class="button tiny" href="#" ng-click="enableEditor()">Edit</a>' +
+            '</div>' +
+            '<div ng-show="view.editorEnabled">' +
+                '<textarea rows="5" cols="50" class="small-12.columns" ng-model="view.editableValue"/>' +
+                
+                '<a class="button tiny" href="#" ng-click="save()">Save</a>' +
+                
+                ' or ' +
+                
+                '<a class="button tiny" href="#" ng-click="disableEditor()">Cancel</a>' +
+            '</div>' +
+        '</div>';
+
+    return {
+        restrict: "A",
+        replace: true,
+        template: editorTemplate,
+        scope: {
+            value: "=clickToEdit",
+        },
+        link: function (scope, element, attrs) {
+            scope.view = {
+                editableValue: scope.value,
+                editorEnabled: false
+            };
+
+            scope.enableEditor = function () {
+                scope.view.editorEnabled = true;
+                scope.view.editableValue = scope.value;
+                setTimeout(function () {
+                    element.find('input')[0].focus();
+                    //element.find('input').focus().select(); // w/ jQuery
+                });
+            };
+
+            scope.disableEditor = function () {
+                scope.view.editorEnabled = false;
+            };
+
+            scope.save = function () {
+                scope.value = scope.view.editableValue;
+                scope.disableEditor();
+            };
+
+        }
+    };
+});
